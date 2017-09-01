891b38cf2735c126efac03c03251582bc7a817ca
Jimmy <jameskinyua590@gmail.com>
2017-09-01 09:10:29 +0300

    [CHORE] Define an event handler handleClick in Register.js

--- src/Register.js
+++ src/Register.js
@@ -4,6 +4,7 @@ import AppBar from 'material-ui/AppBar';
 import RaiseButton from 'material-ui/RaisedButton';
 import TextField from 'material-ui/TextField';
 import axios from 'axios';
+import Login from './Login'
 
 class Register extends Component {
     constructor(props) {
@@ -49,6 +50,38 @@ class Register extends Component {
                 </MuiThemeProvider>
             </div>
         );
+    }
+
+    handleClick(event) {
+        var apiBaseUrl = "http://localhost:5000/api/v1/";
+
+        console.log("values", this.state.username, this.state.password);
+        // To be done, check for empty values before hitting submit
+        var self = this;
+        var payload = {
+            "username": this.state.username,
+            "password": this.state.password,
+            "confirm_password": this.state.confirm_password
+        };
+        axios.post(apiBaseUrl + 'auth/register/', payload)
+            .then(function (response) {
+                console.log(response);
+                if (response.data.code == 200) {
+                    console.log("registration successful");
+                    var loginscreen = [];
+                    loginscreen.push(<Login parentContext={this} />);
+                    var loginmessage = "Not Registered yet. Go to registration";
+                    self.props.parentContext.setState({
+                        loginscreen: loginscreen,
+                        loginmessage: loginmessage,
+                        buttonLabel: "Register",
+                        isLogin: true
+                    });
+                }
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
     }
 }
 
