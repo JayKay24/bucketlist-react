aabdf2bb7b9a5c6fc27a6519d3107ebaea3fee67
Jimmy <jameskinyua590@gmail.com>
2017-09-13 12:44:19 +0300

    [CHORE] Import index.css from the css folder

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 // import { Route, IndexRoute } from 'react-router';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './css/index.css';
 // import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 // import React from 'react';
