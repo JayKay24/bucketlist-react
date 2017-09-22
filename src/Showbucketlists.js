7c5274e6649c8f895ae319db20b1cd0cc32659da
Jimmy <jameskinyua590@gmail.com>
2017-09-22 11:13:11 +0300

    [CHORE] Define a callback function to show bucketlist items

--- src/Showbucketlists.js
+++ src/Showbucketlists.js
@@ -5,6 +5,8 @@ import AppBar from 'material-ui/AppBar';
 import FlatButton from 'material-ui/FlatButton';
 import axios from 'axios';
 import './css/ShowBucketLists.css';
+import NavigationDrawer from './MenuDrawer';
+import RaisedButton from 'material-ui/RaisedButton';
 
 const URL = 'http://localhost:5000/api/v1/bucketlists/'
 
@@ -14,18 +16,22 @@ class ShowBucketlists extends Component {
         this.state = {
             bucketlists: [],
             access_token: '',
+            user_name: '',
             error: ''
         }
         this.getToken = this.getToken.bind(this);
         this.deleteBucketList = this.deleteBucketList.bind(this);
         this.editBucketList = this.editBucketList.bind(this);
+        this.showItems = this.showItems.bind(this);
+        this.handleAdd = this.handleAdd.bind(this);
     }
 
     editBucketList(id, name) {
         console.log(id);
         window.sessionStorage.bkt_id = id;
         window.sessionStorage.bkt_name = name;
-        this.props.history.push(`/edit-bucketlists/${id}`);
+        const userName = window.sessionStorage.userName;
+        this.props.history.push(`/${userName}/edit-bucketlists/${id}`);
     }
 
     deleteBucketList(id) {
@@ -39,11 +45,24 @@ class ShowBucketlists extends Component {
             ).bind(this);
     }
 
+    showItems(id){
+        console.log(id);
+        window.sessionStorage.bkt_id = id;
+        this.props.history.push(`/${window.sessionStorage.userName}/${id}/show-items`);
+    }
+
+    handleAdd(event) {
+        this.props.history.push(`/${this.state.user_name}/add-bucketlist`);
+    }
+
+
     getToken() {
         let access_token = window.sessionStorage.access_token;
+        let user_name = window.sessionStorage.userName;
         if (access_token) {
             this.setState({
-                access_token: access_token
+                access_token: access_token,
+                user_name: user_name
             });
         } else {
             this.setState({
@@ -53,6 +72,7 @@ class ShowBucketlists extends Component {
     }
     componentDidMount() {
         this.getToken();
+
         axios({
             method: 'get',
             url: URL,
@@ -76,7 +96,11 @@ class ShowBucketlists extends Component {
             <div className="ShowBucketLists">
                 <MuiThemeProvider>
                     <div>
+                        <div>
+                        <NavigationDrawer/>
+                        </div>
                         <AppBar title="All Bucket lists" />
+                        <RaisedButton label="Add Bucket list" primary={true} onClick={(event) => this.handleAdd(event)} />
                         <h2>Here are your current bucket lists</h2>
                         <div>
                             {data.map((bucketlist) =>
@@ -85,6 +109,7 @@ class ShowBucketlists extends Component {
                                     <CardActions>
                                         <FlatButton label="Delete" onClick={(event) => this.deleteBucketList(bucketlist.id)} />
                                         <FlatButton label="Edit" onClick={(event) => this.editBucketList(bucketlist.id, bucketlist.bkt_name)} />
+                                        <FlatButton label="Show Items" onClick={(event) => this.showItems(bucketlist.id)} />
                                     </CardActions>
                                 </Card>
                             )}
